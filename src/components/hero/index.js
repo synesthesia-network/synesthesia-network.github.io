35fbc748b086ece88013d30f0034cbbcf53de47f
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-12-02 12:16:57 -0800

    better hero when javascript is disabled

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -36,19 +36,38 @@ export default function Hero() {
 			</div>
 			<div id="animationWrapper" className={styles.animationWrapper}>
 				<div id="blockAnimation" className={styles.blockAnimation}>
+					<noscript>
+					{/* When JavaScript is disabled, show a static logo with colors. */}
 					<svg preserveAspectRatio="xMinYMin meet" viewBox="0 0 220 220" xmlns="http://www.w3.org/2000/svg">
 						<g stroke="rgba(180,180,180,.25)" fill="rgba(0,0,0,0)" transform="translate(10,10)">
-							<polygon transform="translate(0,100)" points="0 50, 100 100, 200 50, 100 0" />
-							<polygon transform="translate(0,90)" points="0 50, 100 100, 200 50, 100 0" />
-							<polygon transform="translate(0,80)" points="0 50, 100 100, 200 50, 100 0" />
-							<polygon transform="translate(0,70)" points="0 50, 100 100, 200 50, 100 0" />
-							<polygon transform="translate(0,60)" points="0 50, 100 100, 200 50, 100 0" />
-							<polygon transform="translate(0,50)" points="0 50, 100 100, 200 50, 100 0" />
-							<polygon transform="translate(0,40)" points="0 50, 100 100, 200 50, 100 0" />
-							<polygon transform="translate(0,30)" points="0 50, 100 100, 200 50, 100 0" />
-							<polygon transform="translate(0,20)" points="0 50, 100 100, 200 50, 100 0" />
-							<polygon transform="translate(0,10)" points="0 50, 100 100, 200 50, 100 0" />
-							<polygon transform="translate(0,0)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(0,100%,50%,.5)" transform="translate(0,100)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(36,100%,50%,.5)" transform="translate(0,90)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(72,100%,50%,.5)" transform="translate(0,80)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(108,100%,50%,.5)" transform="translate(0,70)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(144,100%,50%,.5)" transform="translate(0,60)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(180,100%,50%,.5)" transform="translate(0,50)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(216,100%,50%,.5)" transform="translate(0,40)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(252,100%,50%,.5)" transform="translate(0,30)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(288,100%,50%,.5)" transform="translate(0,20)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(324,100%,50%,.5)" transform="translate(0,10)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(360,100%,50%,.5)" transform="translate(0,0)" points="0 50, 100 100, 200 50, 100 0" />
+						</g>
+					</svg>
+					</noscript>
+					{/* When JavaScript is enabled, show a static logo where the lines are hidden to start. JavaScript will then animate them. */}
+					<svg preserveAspectRatio="xMinYMin meet" viewBox="0 0 220 220" xmlns="http://www.w3.org/2000/svg">
+						<g stroke="rgba(180,180,180,.25)" fill="rgba(0,0,0,0)" transform="translate(10,10)">
+							<polygon stroke="hsla(0,100%,50%,0)" transform="translate(0,100)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(36,100%,50%,0)" transform="translate(0,90)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(72,100%,50%,0)" transform="translate(0,80)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(108,100%,50%,0)" transform="translate(0,70)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(144,100%,50%,0)" transform="translate(0,60)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(180,100%,50%,0)" transform="translate(0,50)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(216,100%,50%,0)" transform="translate(0,40)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(252,100%,50%,0)" transform="translate(0,30)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(288,100%,50%,0)" transform="translate(0,20)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(324,100%,50%,0)" transform="translate(0,10)" points="0 50, 100 100, 200 50, 100 0" />
+							<polygon stroke="hsla(360,100%,50%,0)" transform="translate(0,0)" points="0 50, 100 100, 200 50, 100 0" />
 						</g>
 					</svg>
 				</div>
